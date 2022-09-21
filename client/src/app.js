59dd7472dd6115be05ccc4781e8bddf47a7a3ef0
Jordan Swartz <jordanlswartz@gmail.com>
2022-09-21 09:57:26 -0700

    added another component
    Co-Authored-By: Aryan Binazir <65728619+aryan-binazir@users.noreply.github.com>

--- client/src/app.js
+++ client/src/app.js
@@ -5,40 +5,24 @@ import Product from "./components/product";
 //import data from "../mockData/comments";
 
 const App = () => {
+  const [dummyData, setDummyData] = useState([
+    { name: "Amazon Kindle E-reader", price: "$79.99", quantity: 5 },
+    { name: "Apple 10.5-Inch iPad Pro", price: "$649.99", quantity: 2 },
+    { name: "Yamaha Portable Keyboard", price: "$155.99", quantity: 0 }
+  ]);
   return (
     <div id="app">
         <Header/>
       <main>
       <div className="product-listing">
         <h2>Products</h2>
-        <Product name="Amazon Kindle E-reader" price="$79.99" quantity={5}/>
-        <Product name="Apple 10.5-Inch iPad Pro" price="$649.99" quantity={2}/>
-        <Product name="Yamaha Portable Keyboard" price="$155.99" quantity={0}/>
-      <div className="add-form">
-        <p><a className="button add-product-button">Add A Product</a></p>
-        <h3>Add Product</h3>
-        <form>
-          <div className="input-group">
-            <label label="product-name">Product Name</label>
-            <input type="text" id="product-name" value=""/>
-          </div>
-
-          <div className="input-group">
-            <label label="product-price">Price</label>
-            <input type="text" id="product-price" value=""/>
-          </div>
-
-          <div className="input-group">
-            <label label="product-quantity">Quantity</label>
-            <input type="text" id="product-quantity" value=""/>
-          </div>
-
-          <div className="actions form-actions">
-            <a className="button">Add</a>
-            <a className="button">Cancel</a>
-          </div>
-        </form>
-      </div>
+        <ul>
+        {dummyData.map(product => 
+          <li>
+            <Product {...product}/>
+          </li>
+        )}
+        </ul>
     </div>
       </main>
   </div>
